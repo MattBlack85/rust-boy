5584384bc033a7d2ae6c15f775fb2466427010ec
Mattia Procopio <promat85@gmail.com>
2022-06-19 22:15:00 +0200

    Implement all read/write method for all registers

--- src/registers.rs
+++ src/registers.rs
@@ -9,15 +9,15 @@
 ///  PC    -    -    Program Counter/Pointer
 pub struct Registers {
     a: u8,
-    pub b: u8,
-    pub c: u8,
-    pub d: u8,
-    pub e: u8,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
     f: u8,
-    pub h: u8,
-    pub l: u8,
-    pub sp: u16,
-    pub pc: u16,
+    h: u8,
+    l: u8,
+    sp: u16,
+    pc: u16,
 }
 
 impl Registers {
@@ -66,6 +66,35 @@ impl Registers {
         self.f
     }
 
+    pub fn set_l(&mut self, val: u8) -> u8 {
+        self.l = val;
+        self.f
+    }
+
+    pub fn set_af(&mut self, val: u16) {
+        self.a = (val >> 8) as u8;
+        self.f = (val & 0xFF) as u8;
+    }
+
+    pub fn set_bc(&mut self, val: u16) {
+        self.b = (val >> 8) as u8;
+        self.c = (val & 0xFF) as u8;
+    }
+
+    pub fn set_de(&mut self, val: u16) {
+        self.d = (val >> 8) as u8;
+        self.e = (val & 0xFF) as u8;
+    }
+
+    pub fn set_hl(&mut self, val: u16) {
+        self.h = (val >> 8) as u8;
+        self.l = (val & 0xFF) as u8;
+    }
+
+    pub fn set_sp(&mut self, val: u16) {
+        self.sp = val;
+    }
+
     pub fn read_a(&self) -> u8 {
         self.a
     }
@@ -90,8 +119,32 @@ impl Registers {
         self.f
     }
 
+    pub fn read_h(&self) -> u8 {
+        self.h
+    }
+
+    pub fn read_l(&self) -> u8 {
+        self.l
+    }
+
     pub fn read_af(&self) -> u16 {
         (self.a as u16) << 8 | self.f as u16
+    }
+
+    pub fn read_bc(&self) -> u16 {
+        (self.b as u16) << 8 | self.c as u16
+    }
+
+    pub fn read_de(&self) -> u16 {
+        (self.d as u16) << 8 | self.e as u16
+    }
+
+    pub fn read_hl(&self) -> u16 {
+        (self.h as u16) << 8 | self.l as u16
+    }
+
+    pub fn read_sp(&self) -> u16 {
+        self.sp
     }
 }
 
@@ -102,10 +155,30 @@ mod tests {
     #[test]
     fn check_16bit_registers() {
         let mut r = Registers::new();
-        r.set_a(0xD2);
-        r.set_f(0x40);
 
+        assert_eq!(r.read_af(), 0x0000);
+        r.set_af(0xD240);
+        assert_eq!(r.read_a(), 0xD2);
+        assert_eq!(r.read_f(), 0x40);
         assert_eq!(r.read_af(), 0xD240);
+
+        assert_eq!(r.read_bc(), 0x0000);
+        r.set_bc(0xAF22);
+        assert_eq!(r.read_b(), 0xAF);
+        assert_eq!(r.read_c(), 0x22);
+        assert_eq!(r.read_bc(), 0xAF22);
+
+        assert_eq!(r.read_d(), 0x0000);
+        r.set_de(0x09F1);
+        assert_eq!(r.read_d(), 0x09);
+        assert_eq!(r.read_e(), 0xF1);
+        assert_eq!(r.read_de(), 0x09F1);
+
+        assert_eq!(r.read_hl(), 0x0000);
+        r.set_hl(0x0110);
+        assert_eq!(r.read_h(), 0x01);
+        assert_eq!(r.read_l(), 0x10);
+        assert_eq!(r.read_hl(), 0x0110);
     }
 
     #[test]
@@ -154,5 +227,14 @@ mod tests {
         r.set_f(0xF9);
 
         assert_eq!(r.read_f(), 0xF9);
+    }
+
+    #[test]
+    fn test_sp_register() {
+        let mut r = Registers::new();
+
+        assert_eq!(r.read_sp(), 0x0000);
+        r.set_sp(0xF94A);
+        assert_eq!(r.read_sp(), 0xF94A);
     }
 }
